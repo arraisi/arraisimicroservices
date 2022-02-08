73c38e6ce8b3e393d3a2e55a6654d64e856410a5
Abdul R. Arraisi <arraisi@Abduls-MacBook-Pro.local>
2022-02-08 20:29:38 +0700

    update loggin

--- fraud/src/main/java/com/arraisicode/fraud/controller/FraudCheckHistoryController.java
+++ fraud/src/main/java/com/arraisicode/fraud/controller/FraudCheckHistoryController.java
@@ -3,8 +3,10 @@ package com.arraisicode.fraud.controller;
 import com.arraisicode.fraud.service.FraudCheckService;
 import com.arraisicode.fraud.controller.response.FraudCheckResponse;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping("api/v1/fraud-check")
 @AllArgsConstructor
@@ -16,6 +18,7 @@ public class FraudCheckHistoryController {
     public FraudCheckResponse isFraudster(@PathVariable("customerId") Integer customerID) {
         boolean isFraudulentCustomer = fraudCheckService.
                 isFraudulentCustomer(customerID);
+        log.info("fraud check request for customer {}", customerID);
         return new FraudCheckResponse(isFraudulentCustomer);
     }
 }
